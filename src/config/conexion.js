86656c51cf84779e9f85bcba8bf7c6ebd116a2d1
SairMezaG <z.meza.g@hotmail.com>
2024-07-22 11:13:45 -0500

    cuarto commit

--- src/config/conexion.js
+++ src/config/conexion.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
-    const DB_URI = process.env.DB_URI;
+    const DB_URI = process.env.MONGO_URI;
     try {
         await mongoose.connect(DB_URI);
         console.log("*** Conexión Exitosa ***");
